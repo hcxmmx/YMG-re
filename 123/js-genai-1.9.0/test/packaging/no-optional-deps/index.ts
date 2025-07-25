c160be356236cf90c5947dd3f585bfc8a0298248
hcxmmx <2073152301@qq.com>
2025-07-26 01:31:52 +0800

    chore: remove example files causing build failure

--- 123/js-genai-1.9.0/test/packaging/no-optional-deps/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2025 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-import {GoogleGenAI} from '@google/genai';
-
-// If it's unused the formatter removes the import.
-console.log(GoogleGenAI);
